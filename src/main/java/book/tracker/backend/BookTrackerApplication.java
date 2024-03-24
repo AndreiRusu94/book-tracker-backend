00763a122506e84874e2ffa961d192abc64cf584
Andrei Rusu <andrei.rusu2094@gmail.com>
2024-03-24 17:12:11 +0200

    Setup the maven project configuration

---
+++ src/main/java/book/tracker/backend/BookTrackerApplication.java
@@ -0,0 +1,12 @@
+package book.tracker.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookTrackerApplication.class, args);
+    }
+}
